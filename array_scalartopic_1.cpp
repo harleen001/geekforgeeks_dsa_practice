a03ff07572b3ff4609ce18f045920b79b4340421
harleen <harleen.johal31@gmail.com>
2024-10-25 17:41:16 +0530

    array-scalar-1

---
+++ array_scalartopic_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={4,1,7,5,10};
+    int sum = 0;
+    for (int i=0;i<5;i++)
+    {
+        sum += arr[i];
+        
+    }
+    cout<<sum;
+
+}
