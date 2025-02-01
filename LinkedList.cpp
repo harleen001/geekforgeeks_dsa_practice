7809e579859153059c3f79020d846eac55319307
harleen001 <harleen.johal31@gmail.com>
2025-02-01 17:32:11 +0530

    LL-Insert@Starting

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,2 +1,26 @@
-//Array -> contugious memory
+//Array -> contugious memory, random access
 //LL -> memory but not contigious
+
+class Node{
+    int data; //data
+    Node next; //next node address
+}
+
+Node n1=new Node(10);
+ //OR
+Node n1=new Node();
+n1.data=10;
+
+Node n2=new Node();
+n2.data=20;
+
+n1.next=n2; // creates a linkage between n1 and n2
+
+
+//While inserting element at starting lets say insert 5 at starting
+Node addFirst(value)
+{Node n1=new Node(value);
+n1.next=head;//referenced
+head=n1;//new head given
+return head;
+}
